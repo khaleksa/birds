ea0053258b6db7f130781c52b63a483842c16b1c
Alexandra Khegay <khaleksa@gmail.com>
2014-08-24 13:33:04 +0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birds::Application.config.secret_key_base = '37210ff7dfad1267cc72188237932679f091669a7cdc3b84952e7a1b785ee7530a79df8a81a7ad403e1b27b20141b640fb1c8e7a462a45df0527ac114b746bf5'
