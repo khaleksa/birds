91b0c7cc1b1a1263488743d3f10ebf9991bbfceb
Alexandra Khegay <khaleksa@gmail.com>
2015-02-15 10:57:35 +0500

    add fancybox jquery plagin for species image view

--- app/assets/javascripts/pages/species.js
+++ app/assets/javascripts/pages/species.js
@@ -1,10 +1,25 @@
 $(function() {
     $('.thumb_container .img-responsive').on('click', function(event) {
         image = $(event.target)
-        $('.bird-photo-img .img-responsive').attr('src', image.data('src'));
+        main_image = $('.bird-photo-img .img-responsive')
+        main_image.attr('src', image.data('src'));
+
+        main_image_parent = main_image.parent()
+        main_image_parent.attr('href', image.data('src'))
+        main_image_parent.attr('title', image.data('description'))
+
         $('#photo_descr_text').text(image.data('description'));
         $('#photo_profile_lnk').text(image.data('author'));
         $('#photo_date').text(image.data('date'));
         $('#photo_place').text(image.data('place'));
+    });
+
+    //Init fancybox, ref - http://fancyapps.com/fancybox/
+    $("#fancybox_single_image").fancybox({
+        helpers: {
+            title : {
+                type : 'float'
+            }
+        }
     });
 });
