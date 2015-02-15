90d574ac305a52867eeb9fecbd8c77163aa061bd
Alexandra Khegay <khaleksa@gmail.com>
2015-02-15 17:47:08 +0500

    set active menu in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def ldate(dt, hash = {})
     dt ? l(dt, hash) : nil
   end
+
+  def nav_link_to(name = nil, options = nil, html_options = nil, &block)
+    is_active = request.path.start_with?(url_for(options)) ? true : false
+
+    li_class = []
+    li_class.append(html_options.delete(:li_class)) if html_options
+    li_class.append(:active) if is_active
+
+    content_tag :li, class: li_class.presence do
+      link_to(name, options, html_options, &block)
+    end
+  end
 end
