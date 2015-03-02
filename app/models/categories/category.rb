71f4013bbb6f599863bfcf686fffc0198ed6ddad
Alexandra Khegay <khaleksa@gmail.com>
2015-03-02 22:01:48 +0500

    set order of orders & families in active admin selects

--- app/models/categories/category.rb
+++ app/models/categories/category.rb
@@ -1,13 +1,12 @@
 class Categories::Category < ActiveRecord::Base
   validates_presence_of :name_ru, :name_lat, :name_en, :name_uz
-  validates_uniqueness_of :name_ru, :name_lat, :name_en, :name_uz
 
   acts_as_tree :order => 'name_ru'
 
   mount_uploader :image, CategoryUploader
 
-  scope :orders, -> { where(type: 'Categories::Order') }
-  scope :families, -> { where(type: 'Categories::Family') }
+  scope :orders, -> { where(type: 'Categories::Order').order(:name_ru) }
+  scope :families, -> { where(type: 'Categories::Family').order(:name_ru) }
 
   def full_name
     result = ''
