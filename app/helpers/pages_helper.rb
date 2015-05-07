c5844a3a3cbc7b9e15f3b66819a3ffb57bf7b89c
Alexandra Khegay <khaleksa@gmail.com>
2015-05-07 22:42:46 +0500

    show comment's date

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,4 +3,13 @@ module PagesHelper
   def member_column_count(total_count)
     (total_count < 3) ? total_count : 3
   end
+
+  def hide_body_backgroud?
+    none_background_pages_options.detect { |options| current_page?(options) }
+  end
+
+  private
+  def none_background_pages_options
+    [ {controller: 'pages', action: 'index'} ]
+  end
 end
