4897748749a384f99e201443ca28cc0ad3cfd52e
Alexandra Khegay <khaleksa@gmail.com>
2015-05-09 12:24:45 +0500

    fix layout

--- app/assets/javascripts/pages/common.js
+++
@@ -1,8 +0,0 @@
-////TODO
-$(function() {
-    var window_height = jQuery(window).height() - 190;
-    var content_height = jQuery('.container.content').height();
-    if (content_height < window_height) {
-        $('.container.content').height(window_height);
-    }
-});
