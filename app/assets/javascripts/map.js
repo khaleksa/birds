4cf00a2ac3f319301b15f16a008114dca729f2ee
Alexandra Khegay <khaleksa@gmail.com>
2015-06-15 13:29:05 +0500

    js: create custom object for google maps

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,56 +1,119 @@
-$(function() {
-    var bird_add_map;
-    var markers = [];
+var gmap = {
+    map: null,
+    bounds: null,
+    markers : [],
+    infowindow: null
+}
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+gmap.init = function(selector, latLng, zoom) {
+    var mapOptions = {
+        zoom: zoom,
+        center: latLng,
+        mapTypeControl: true,
+        mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
+        navigationControl: true,
+        navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map($(selector)[0], mapOptions);
+}
 
-    function initialize() {
-        var map_element = $('#map_canvas');
-        var edit_mode = map_element.data('edit');
+gmap.placeMarker = function(latLng) {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: this.map
+    });
 
+    this.markers.push(marker);
+    marker
+}
+
+// Deletes all markers in the array by removing references to them
+gmap.clearMarkers = function() {
+    if (this.markers) {
+        for (i in this.markers) {
+            this.markers[i].setMap(null);
+        }
+        this.markers.length = 0;
+    }
+}
+
+gmap.initInfoWindow = function() {
+    this.infowindow =  new google.maps.InfoWindow({
+        content: ""
+    })
+}
+
+gmap.placeMarkerWithInfo = function(data) {
+    var contentString = '<div id="map_content" class="map-content">'+
+        '<div class="map_left"><img class="img-responsive" src="'+data.img+'" /></div>'+
+        '<div class="map_right">'+
+        '<a href="'+data.auth_link+'">'+data.auth+'</a>'+
+        '<p>'+data.date+'</p>'+
+        '<p>'+data.place+'</p>'+
+        '</div></div>';
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.lng),
+        map: this.map,
+        content: contentString
+    });
+    this.markers.push(marker);
+
+    $this = this
+    google.maps.event.addListener(marker, 'click', function () {
+        $this.infowindow.setContent(this.content);
+        $this.infowindow.open($this.map, this);
+    });
+}
+
+$(document).ready(function() {
+    var map_element = $('#map_canvas');
+    var edit_mode = map_element.data('edit');
+
+    var markers = [
+        {
+            "lat": 40.31139,
+            "lng": 68.27972,
+            "img" : 'assets/img/bird_example_small.png',
+            "auth" : 'Мария Иванова',
+            "auth_link" : '#',
+            "date" : '11 июля 2014',
+            'place' : 'Ташкент, Сквер'
+        },
+        {
+            "lat": 41.31059,
+            "lng": 69.26073,
+            "img" : 'assets/img/bird_example_small.png',
+            "auth" : 'Дина',
+            "auth_link" : '#',
+            "date" : '11 июля 2014',
+            'place' : 'Ташкент, Проспект Узбекистан'
+
+        }
+    ];
+
+    if (map_element.data('markers')) {
+        gmap.init(map_element, {lat: 41.27, lng: 69.23}, 8)
+        gmap.initInfoWindow();
+
+        for (var i = 0, length = markers.length; i < length; i++) {
+            gmap.placeMarkerWithInfo(markers[i]);
+        }
+    } else {
         if (edit_mode) {
-            var mapOptions = {
-                center: { lat: 41.27, lng: 69.23},
-                zoom: 8
-            };
-
-            bird_add_map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-            google.maps.event.addListener(bird_add_map, "click", function (event) {
+            gmap.init(map_element, {lat: 41.27, lng: 69.23}, 8)
+            google.maps.event.addListener(gmap.map, "click", function (event) {
                 placeLanLng(event.latLng);
-                placeMarker(event.latLng);
+                gmap.clearMarkers();
+                gmap.placeMarker(event.latLng);
             });
         } else {
             var latitude = map_element.data('lat');
             var longitude = map_element.data('lng');
-            var mapOptions = {
-                center: { lat: latitude, lng: longitude},
-                zoom: 8
-            };
-            bird_add_map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-            placeMarker({lat: latitude, lng: longitude});
-        }
-    }
-
-    function placeMarker(latLng) {
-        clearMarkers();
-
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: bird_add_map,
-        });
-
-        markers.push(marker);
-    }
-
-    // Deletes all markers in the array by removing references to them
-    function clearMarkers() {
-        if (markers) {
-            for (i in markers) {
-                markers[i].setMap(null);
-            }
-            markers.length = 0;
+            gmap.init(map_element, {lat: latitude, lng: longitude}, 8)
+            gmap.placeMarker({lat: latitude, lng: longitude});
         }
     }
 
@@ -60,4 +123,3 @@ $(function() {
     }
 
 });
-
