c32e017b315e398eba763a2922f4eff20fa05a64
Alexandra Khegay <khaleksa@gmail.com>
2015-08-01 17:32:02 +0500

    make species search case insensitive

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
 
   def search
     return if params[:text].blank?
-    @species = Species.by_name(sanitize_text(params[:text])).order(:name_ru).all
+    @species = species_by_name
     respond_to do |format|
       format.js
     end
@@ -14,14 +14,23 @@ class SearchController < ApplicationController
 
   def search_main_species
     return if params[:text].blank?
-    @species = Species.by_name(sanitize_text(params[:text])).main.order(:name_ru).all
+    @species = species_by_name(true)
     respond_to do |format|
       format.js
     end
   end
 
   private
+  def species_by_name(only_main = false)
+    @species = Species
+                   .where("(lower(name_ru) LIKE :name) OR (lower(name_en) LIKE :name) OR (lower(name_lat) LIKE :name)",
+                          name: sanitize_text(params[:text]))
+                   .order(:name_ru)
+    @species = @species.main if only_main
+    @species
+  end
+
   def sanitize_text(text)
-    '%' + text.to_s.downcase + '%'
+    '%' + text.mb_chars.downcase.to_s + '%'
   end
 end
