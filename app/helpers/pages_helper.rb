e7ca78406872edc01b571db6d916c83e1ae0a566
Alexandra Khegay <khaleksa@gmail.com>
2015-11-25 14:36:06 +0500

    update date of Best photo contest, add photo for FB

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,23 @@
 module PagesHelper
 
+  FACEBOOK_PHOTOS = {
+      'default' => 'fb/birds-fb-share.png',
+      'big_day_in_tashkent' => 'fb/big-day-2015.png',
+      'best_photo' => 'fb/best-photo-fb.png'
+  }
+
+  FACEBOOK_DESCRIPTION = {
+      'default' => 'Здесь вы найдете информацию о птицах Узбекистана и много фотографий! Сайт создан людьми, увлеченными птицами и природой, которые готовы помочь каждому в определении птиц.',
+      'big_day_in_tashkent' => 'Любите природу и прогулки по свежему воздуху? Тогда вам наверняка должно понравиться наблюдение за птицами или бердвотчинг. Примите участие в соревновании «Большой День»!',
+      'best_photo' => 'Мы верим, что фотографирование птиц - лучшая альтернативой охоте на них, что путешествовать по нашей стране интересно, а бердвотчинг познакомит вас с природой и новыми интересными людьми!'
+  }
+
+  FACEBOOK_TITLE = {
+      'default' => 'Давайте наблюдать птиц вместе!',
+      'big_day_in_tashkent' => 'Большой День в Ташкенте, 15 ноября 2015',
+      'best_photo' => 'Лучшая фотография птицы Узбекистана'
+  }
+
   def member_column_count(total_count)
     (total_count < 3) ? total_count : 3
   end
@@ -9,26 +27,26 @@ module PagesHelper
   end
 
   def og_image_url
-    if controller_name == 'contests' && action_name == 'big_day_in_tashkent'
-      URI.join(root_url, image_path('fb/big-day-2015.png'))
+    if controller_name == 'contests' && FACEBOOK_PHOTOS.keys.include?(action_name)
+      URI.join(root_url, image_path(FACEBOOK_PHOTOS[action_name]))
     else
-      URI.join(root_url, image_path('fb/birds-fb-share.png'))
+      URI.join(root_url, image_path(FACEBOOK_PHOTOS['default']))
     end
   end
 
   def og_title
-    if controller_name == 'contests' && action_name == 'big_day_in_tashkent'
-      'Большой День в Ташкенте, 15 ноября 2015'
+    if controller_name == 'contests' && FACEBOOK_PHOTOS.keys.include?(action_name)
+      FACEBOOK_TITLE[action_name]
     else
-      ''
+      FACEBOOK_TITLE['default']
     end
   end
 
   def og_description
-    if controller_name == 'contests' && action_name == 'big_day_in_tashkent'
-      'Любите природу и прогулки по свежему воздуху? Тогда вам наверняка должно понравиться наблюдение за птицами или бердвотчинг. Примите участие в соревновании «Большой День»!'
+    if controller_name == 'contests' && FACEBOOK_PHOTOS.keys.include?(action_name)
+      FACEBOOK_DESCRIPTION[action_name]
     else
-      'Здесь вы найдете информацию о птицах Узбекистана и много фотографий! Сайт создан людьми, увлеченными птицами и природой, которые готовы помочь каждому в определении птиц.'
+      FACEBOOK_DESCRIPTION['default']
     end
   end
 
