cf9347f7dc2eaff366b03d6e6f5f549102fcd3f3
Alexandra Khegay <khaleksa@gmail.com>
2016-03-31 23:07:27 +0500

    add birthday theme

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -64,6 +64,8 @@ module PagesHelper
       "#{current_user.first_name}, с Днем Рожденья! Birds.uz :)"
     elsif ['30.12.2015'.to_date, '31.12.2015'.to_date, '1.01.2016'.to_date].include?(Time.current.to_date) && controller_name == 'pages' && action_name == 'index'
       'С Новым Годом! С любовью, команда Birds.uz'
+    elsif Time.current.month == 4 && [1..7].include?(Time.current.day)
+      '1 апреля - День рождения Birds.uz!<br>Благодарим всех за этот увлекательный год и поздравляем вас с Днем Птиц!'
     else
       'Birds.uz – давайте наблюдать птиц Узбекистана вместе!'
     end
