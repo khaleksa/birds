6a0ce84a0cfd0c61745080a60cc95aa390d1b8f1
Alexandra Khegay <alexandra@hackerone.com>
2019-07-14 16:36:04 +0200

    precompile map.js in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,6 @@ Birds::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.assets.precompile += ['map.js']
 end
