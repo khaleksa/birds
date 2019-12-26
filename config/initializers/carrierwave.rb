5b1d9441de024ab507ce6b4e6222ba1b371f4d64
Alexandra Khegay <alexandra@hackerone.com>
2019-12-26 16:06:58 +0100

    add gc bucket credentials, config carrierwave with google cloud

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,14 @@
+# TODO: separate in 2 files
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/google'
+  config.fog_credentials = {
+      provider: 'Google',
+      google_project: 'sonorous-mix-245813',
+      google_json_key_location: '/Users/alexandra/XProject/birds/birds/google_cred.json'
+  }
+  config.fog_directory = 'birdsuzb_images'
+end
+
 module CarrierWave
   module MiniMagick
     def quality(percentage)
