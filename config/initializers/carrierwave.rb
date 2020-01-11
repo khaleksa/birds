07e4a10069356136a5f775f5f9f079244d02b3c1
Alexandra Khegay <alexandra@hackerone.com>
2020-01-11 12:54:00 +0100

    move carrierwave configs to env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,12 @@
-# TODO: separate in 2 files
+require 'carrier_wave/mini_magick'
+
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'
   config.fog_credentials = {
       provider: 'Google',
-      google_project: 'sonorous-mix-245813',
-      google_json_key_location: '/Users/alexandra/XProject/birds/birds/google_cred.json'
+      google_project: ENV['GC_BIRDS_PROJECT_ID'],
+      google_json_key_location: ENV['GC_BIRDS_CRED_FILE_PATH']
   }
-  config.fog_directory = 'birdsuzb_images'
+  config.fog_directory = ENV['GC_BIRDS_IMAGE_BUCKET_ID']
 end
 
-module CarrierWave
-  module MiniMagick
-    def quality(percentage)
-      manipulate! do |img|
-        img.quality(percentage.to_s)
-        img = yield(img) if block_given?
-        img
-      end
-    end
-  end
-end
