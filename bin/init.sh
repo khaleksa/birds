6d82abd3c7f0b6cba3760d0e6414ae37f64e8390
Alexandra Khegay <alexandra@hackerone.com>
2020-01-18 18:03:43 +0100

    hardcode all credentials in the source files

--- bin/init.sh
+++ bin/init.sh
@@ -15,3 +15,18 @@ else
 fi
 
 exec $@
+
+#cloud.conf file content:
+#
+#echo 'this is a cloud.conf'
+## Google Cloud settings
+#GC_BIRDS_CRED_FILE_PATH='/Users/alexandra/XProject/birds/birds/google_cred.json'
+#GC_BIRDS_PROJECT_ID='sonorous-mix-245813'
+#GC_BIRDS_IMAGE_BUCKET_ID='birdsuzb_images'
+#
+## Postgres settings. Connect to your dev db using "rails db".
+#DATABASE_PORT=5432
+#DATABASE_HOST=34.89.189.108
+#DATABASE_USERNAME=birds_user
+#DATABASE_PASSWORD=mo2vwzxk5cmnc1a1
+#DATABASE_NAME=birds_production
